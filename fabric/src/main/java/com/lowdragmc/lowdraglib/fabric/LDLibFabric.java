513e21e30ac1d38ddd1cb76ca81d6a44aae94dcd
KilaBash <yefancy@foxmail.com>
2023-02-11 19:12:31 +0800

    gradle

--- fabric/src/main/java/com/lowdragmc/lowdraglib/fabric/LDLibFabric.java
+++ fabric/src/main/java/com/lowdragmc/lowdraglib/fabric/LDLibFabric.java
@@ -3,7 +3,6 @@ package com.lowdragmc.lowdraglib.fabric;
 import com.lowdragmc.lowdraglib.CommonProxy;
 import com.lowdragmc.lowdraglib.LDLib;
 import com.lowdragmc.lowdraglib.ServerCommands;
-import com.lowdragmc.lowdraglib.syncdata.TypedPayloadRegistries;
 import com.lowdragmc.lowdraglib.utils.fabric.ReflectionUtilsImpl;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
@@ -17,10 +16,10 @@ public class LDLibFabric implements ModInitializer {
         ServerLifecycleEvents.SERVER_STARTED.register(server -> PlatformImpl.SERVER = server);
         // register server commands
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> ServerCommands.createServerCommands().forEach(dispatcher::register));
-        // execute annotation searching
-        ReflectionUtilsImpl.execute();
         // init common features
         CommonProxy.init();
+        // execute annotation searching
+        ReflectionUtilsImpl.execute();
     }
 
 }
