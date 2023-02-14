886eb7bca2822432b12688ea80833d496973199c
KilaBash <yefancy@foxmail.com>
2023-02-15 00:46:27 +0800

    fix item group

---
+++ forge/src/main/java/com/lowdragmc/lowdraglib/utils/forge/LDLItemGroupImpl.java
@@ -0,0 +1,12 @@
+package com.lowdragmc.lowdraglib.utils.forge;
+
+/**
+ * @author KilaBash
+ * @date 2023/2/15
+ * @implNote LDLItemGroupImpl
+ */
+public class LDLItemGroupImpl {
+    public static int expandArrayAndGetId() {
+        return  -1;
+    }
+}
