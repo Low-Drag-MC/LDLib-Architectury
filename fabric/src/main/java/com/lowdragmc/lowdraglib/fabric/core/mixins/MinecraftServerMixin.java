2bc6ca85109d14ffc52a6b7d99ddf3152c01ae0c
KilaBash <yefancy@foxmail.com>
2023-04-03 10:35:43 +0800

    Update MinecraftServerMixin.java

--- fabric/src/main/java/com/lowdragmc/lowdraglib/fabric/core/mixins/MinecraftServerMixin.java
+++ fabric/src/main/java/com/lowdragmc/lowdraglib/fabric/core/mixins/MinecraftServerMixin.java
@@ -5,9 +5,8 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-import java.io.IOException;
 
 /**
  * @author KilaBash
@@ -17,10 +16,10 @@ import java.io.IOException;
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin {
 
-    @Redirect(method = "stopServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;close()V"))
-    private void injectPrepare(ServerLevel serverLevel) throws IOException {
+    @ModifyVariable(method = "stopServer", at = @At(value= "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;close()V", ordinal = 0))
+    public ServerLevel MinecraftServer_stopServer(ServerLevel serverLevel) {
         AsyncThreadData.getOrCreate(serverLevel).releaseExecutorService();
-        serverLevel.close();
+        return serverLevel;
     }
 
 }
