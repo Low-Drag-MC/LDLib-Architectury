30fbc38aa0ef0e5134101d94a02f6ea825774a82
KilaBash <yefancy@foxmail.com>
2023-06-14 20:46:57 +0800

    chore(release):1.0.9

--- common/src/main/java/com/lowdragmc/lowdraglib/core/mixins/PackConfigMixin.java
+++ common/src/main/java/com/lowdragmc/lowdraglib/core/mixins/PackConfigMixin.java
@@ -5,7 +5,6 @@ import com.lowdragmc.lowdraglib.utils.CustomResourcePack;
 import net.minecraft.server.WorldLoader;
 import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.resources.ReloadableResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -16,7 +15,12 @@ import java.util.List;
 @Mixin(WorldLoader.PackConfig.class)
 public abstract class PackConfigMixin {
 
-    @ModifyVariable(method = "createResourceManager", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/resources/MultiPackResourceManager;<init>(Lnet/minecraft/server/packs/PackType;Ljava/util/List;)V"), index = 3)
+    @ModifyVariable(method = "createResourceManager",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/server/packs/resources/MultiPackResourceManager;<init>(Lnet/minecraft/server/packs/PackType;Ljava/util/List;)V",
+                    shift = At.Shift.BEFORE,
+                    by = 1
+    ))
     private List<PackResources> injectCreateReload(List<PackResources> resourcePacks) {
         var mutableList = new ArrayList<>(resourcePacks);
         mutableList.add(new CustomResourcePack(LDLib.location, LDLib.MOD_ID, PackType.SERVER_DATA));
