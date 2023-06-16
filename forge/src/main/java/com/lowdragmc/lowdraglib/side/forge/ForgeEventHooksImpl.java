07b80605adf55362a769ff3db26eec32a3aa426f
KilaBash <yefancy@foxmail.com>
2023-06-16 23:28:23 +0800

    making ui open pack using rl + add forge hooks side annotations

--- forge/src/main/java/com/lowdragmc/lowdraglib/side/forge/ForgeEventHooksImpl.java
+++ forge/src/main/java/com/lowdragmc/lowdraglib/side/forge/ForgeEventHooksImpl.java
@@ -6,6 +6,8 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ContainerScreenEvent;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
@@ -22,18 +24,21 @@ public class ForgeEventHooksImpl {
         MinecraftForge.EVENT_BUS.post(new PlayerContainerEvent.Open(player, container));
     }
 
+    @OnlyIn(Dist.CLIENT)
     public static void postBackgroundRenderedEvent(Screen screen, PoseStack poseStack) {
         RenderSystem.depthMask(true);
         net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new ScreenEvent.BackgroundRendered(screen, poseStack));
         RenderSystem.depthMask(false);
     }
 
+    @OnlyIn(Dist.CLIENT)
     public static void postRenderBackgroundEvent(AbstractContainerScreen<?> guiContainer, PoseStack poseStack, int mouseX, int mouseY) {
         RenderSystem.depthMask(true);
         net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new ContainerScreenEvent.Render.Background(guiContainer, poseStack, mouseX, mouseY));
         RenderSystem.depthMask(false);
     }
 
+    @OnlyIn(Dist.CLIENT)
     public static void postRenderForegroundEvent(AbstractContainerScreen<?> guiContainer, PoseStack poseStack, int mouseX, int mouseY) {
         RenderSystem.depthMask(true);
         net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new ContainerScreenEvent.Render.Foreground(guiContainer, poseStack, mouseX, mouseY));
