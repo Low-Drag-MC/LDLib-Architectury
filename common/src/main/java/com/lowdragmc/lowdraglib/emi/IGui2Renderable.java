bedf35d81c60337450699f02c3762e7c7a007bf8
KilaBash <yefancy@foxmail.com>
2023-06-23 21:16:03 +0800

    Update IGui2Renderable.java

--- common/src/main/java/com/lowdragmc/lowdraglib/emi/IGui2Renderable.java
+++ common/src/main/java/com/lowdragmc/lowdraglib/emi/IGui2Renderable.java
@@ -1,14 +1,7 @@
 package com.lowdragmc.lowdraglib.emi;
 
 import com.lowdragmc.lowdraglib.gui.texture.IGuiTexture;
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.Tesselator;
 import dev.emi.emi.api.render.EmiRenderable;
-import me.shedaniel.math.Rectangle;
-import me.shedaniel.rei.api.client.gui.Renderer;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.renderer.MultiBufferSource;
 
 /**
  * @author KilaBash
@@ -17,10 +10,8 @@ import net.minecraft.client.renderer.MultiBufferSource;
  */
 public interface IGui2Renderable {
     static EmiRenderable toDrawable(IGuiTexture guiTexture, int width, int height) {
-        return (matrices, x, y, delta) -> {
+        return (graphics, x, y, delta) -> {
             if (guiTexture == null) return;
-            var graphics = new GuiGraphics(Minecraft.getInstance(), MultiBufferSource.immediate((Tesselator.getInstance().getBuilder())));
-            graphics.pose().mulPoseMatrix(matrices.last().pose());
             guiTexture.draw(graphics, 0, 0, x, y, width, height);
         };
     }
