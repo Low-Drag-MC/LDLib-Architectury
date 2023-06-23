ca7c8c5463c2c2f83e29375e890d82ba4e0b3760
KilaBash <yefancy@foxmail.com>
2023-06-23 14:22:47 +0800

    Create EMIPlugin.java

---
+++ forge/src/main/java/com/lowdragmc/lowdraglib/forge/emi/EMIPlugin.java
@@ -0,0 +1,23 @@
+package com.lowdragmc.lowdraglib.forge.emi;
+
+import com.lowdragmc.lowdraglib.gui.modular.ModularUIEmiHandlers;
+import com.lowdragmc.lowdraglib.gui.modular.ModularUIGuiContainer;
+import dev.emi.emi.api.EmiEntrypoint;
+import dev.emi.emi.api.EmiPlugin;
+import dev.emi.emi.api.EmiRegistry;
+
+/**
+ * @author KilaBash
+ * @date 2023/4/3
+ * @implNote EMIPlugin
+ */
+@EmiEntrypoint
+public class EMIPlugin implements EmiPlugin {
+    @Override
+    public void register(EmiRegistry registry) {
+        registry.addDragDropHandler(ModularUIGuiContainer.class, ModularUIEmiHandlers.DRAG_DROP_HANDLER);
+        registry.addExclusionArea(ModularUIGuiContainer.class, ModularUIEmiHandlers.EXCLUSION_AREA);
+        registry.addStackProvider(ModularUIGuiContainer.class, ModularUIEmiHandlers.STACK_PROVIDER);
+    }
+
+}
