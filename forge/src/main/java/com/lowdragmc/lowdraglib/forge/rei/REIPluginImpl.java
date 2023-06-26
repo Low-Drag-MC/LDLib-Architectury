6fdae1a786f04509c2e268662268d14bdc199571
screret <joona.v.stenberg@gmail.com>
2023-06-26 20:25:22 +0300

    fixed an error on server that was caused by annotating the rei plugin as common when it's client

--- forge/src/main/java/com/lowdragmc/lowdraglib/forge/rei/REIPluginImpl.java
+++ forge/src/main/java/com/lowdragmc/lowdraglib/forge/rei/REIPluginImpl.java
@@ -1,14 +1,14 @@
 package com.lowdragmc.lowdraglib.forge.rei;
 
 import com.lowdragmc.lowdraglib.rei.REIPlugin;
-import me.shedaniel.rei.forge.REIPluginCommon;
+import me.shedaniel.rei.forge.REIPluginClient;
 
 /**
  * @author KilaBash
  * @date 2022/11/27
  * @implNote REIPlugin
  */
-@REIPluginCommon
+@REIPluginClient
 public class REIPluginImpl extends REIPlugin {
 
 }
