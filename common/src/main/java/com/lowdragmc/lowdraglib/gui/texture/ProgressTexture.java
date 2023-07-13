a1bfea9309fb2f414d59a89201389477a6f6bcfe
KilaBash <yefancy@foxmail.com>
2023-07-13 18:20:06 +0800

    Update ProgressTexture.java

--- common/src/main/java/com/lowdragmc/lowdraglib/gui/texture/ProgressTexture.java
+++ common/src/main/java/com/lowdragmc/lowdraglib/gui/texture/ProgressTexture.java
@@ -3,7 +3,6 @@ package com.lowdragmc.lowdraglib.gui.texture;
 import com.lowdragmc.lowdraglib.gui.editor.annotation.Configurable;
 import com.lowdragmc.lowdraglib.gui.editor.annotation.LDLRegister;
 import com.lowdragmc.lowdraglib.gui.editor.data.resource.Resource;
-import com.mojang.blaze3d.vertex.PoseStack;
 import lombok.Getter;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -73,8 +72,21 @@ public class ProgressTexture extends TransformTexture {
             float drawnV = (float) fillDirection.getDrawnV(progress);
             float drawnWidth = (float) fillDirection.getDrawnWidth(progress);
             float drawnHeight = (float) fillDirection.getDrawnHeight(progress);
-            filledBarArea.drawSubArea(graphics, x + drawnU * width, y + drawnV * height, ((int) (width * drawnWidth)), ((int) (height * drawnHeight)),
-                    drawnU, drawnV,((int) (drawnWidth * width)) / (width * 1.0f), ((int) (drawnHeight * height)) / (height * 1.0f));
+            float X = x + drawnU * width;
+            float Y = y + drawnV * height;
+            float W = width * drawnWidth;
+            float H = height * drawnHeight;
+
+            if (fillDirection == FillDirection.LEFT_TO_RIGHT || fillDirection == FillDirection.UP_TO_DOWN) {
+                filledBarArea.drawSubArea(graphics, X, Y, (int) W, (int) H, drawnU, drawnV,
+                        ((drawnWidth * width)) / (width),
+                        ((drawnHeight * height)) / (height));
+            } else {
+                filledBarArea.drawSubArea(graphics, (int) X, (int) Y, width - (int) X, height - (int) Y, drawnU, drawnV,
+                        ((drawnWidth * width)) / (width),
+                        ((drawnHeight * height)) / (height));
+            }
+
         }
     }
 
