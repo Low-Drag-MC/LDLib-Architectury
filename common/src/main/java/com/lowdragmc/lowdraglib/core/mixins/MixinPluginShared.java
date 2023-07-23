9086695acd09584b61523b8e2e74362e8aca05f4
KilaBash <yefancy@foxmail.com>
2023-07-23 21:15:48 +0800

    fix mixin jei checking

--- common/src/main/java/com/lowdragmc/lowdraglib/core/mixins/MixinPluginShared.java
+++ common/src/main/java/com/lowdragmc/lowdraglib/core/mixins/MixinPluginShared.java
@@ -15,7 +15,7 @@ public interface MixinPluginShared {
 	boolean IS_DASH_LOADER = isClassFound("dev.quantumfusion.dashloader.mixin.MixinPlugin");
 
 	boolean IS_SODIUM_LOAD = isClassFound("me.jellysquid.mods.sodium.mixin.SodiumMixinPlugin");
-	boolean IS_JEI_LOAD = isClassFound("mezz.jei.core.config.GiveMode");
+	boolean IS_JEI_LOAD = isClassFound("mezz.jei.core.search.SearchMode");
 	boolean IS_REI_LOAD = isClassFound("me.shedaniel.rei.api.common.entry.EntryStack");
 	boolean IS_MEI_LOAD = isClassFound("dev.emi.emi.api.EmiFillAction");
 	boolean IS_RUBIDIUM_LOAD = IS_SODIUM_LOAD;
