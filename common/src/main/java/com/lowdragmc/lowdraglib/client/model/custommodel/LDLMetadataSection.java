d589c7856dab06861f916ea3de7625849dbf1d08
KilaBash <yefancy@foxmail.com>
2023-11-04 21:32:54 +0900

    fix LDLMetadataSection crash

--- common/src/main/java/com/lowdragmc/lowdraglib/client/model/custommodel/LDLMetadataSection.java
+++ common/src/main/java/com/lowdragmc/lowdraglib/client/model/custommodel/LDLMetadataSection.java
@@ -7,20 +7,20 @@ import com.lowdragmc.lowdraglib.client.model.ModelFactory;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.metadata.MetadataSectionSerializer;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Environment(EnvType.CLIENT)
 public class LDLMetadataSection {
     public static final String SECTION_NAME = LDLib.MOD_ID;
-    private static final Map<ResourceLocation, LDLMetadataSection> METADATA_CACHE = new HashMap<>();
+    private static final Map<ResourceLocation, LDLMetadataSection> METADATA_CACHE = new ConcurrentHashMap<>();
+    public static final LDLMetadataSection MISSING = new LDLMetadataSection(false, null);
 
     public final boolean emissive;
     public final ResourceLocation connection;
@@ -30,12 +30,20 @@ public class LDLMetadataSection {
         this.connection = connection;
     }
 
-    @Nullable
+    public static void clearCache() {
+        METADATA_CACHE.clear();
+    }
+
+    public boolean isMissing() {
+        return this == MISSING;
+    }
+
+    @Nonnull
     public static LDLMetadataSection getMetadata(ResourceLocation res) {
         if (METADATA_CACHE.containsKey(res)) {
             return METADATA_CACHE.get(res);
         }
-        LDLMetadataSection ret = null;
+        LDLMetadataSection ret = MISSING;
         var resourceOptional = Minecraft.getInstance().getResourceManager().getResource(res);
         if (resourceOptional.isPresent()) {
             var resource = resourceOptional.get();
@@ -48,20 +56,20 @@ public class LDLMetadataSection {
         return ret;
     }
 
-    @Nullable
+    @Nonnull
     public static LDLMetadataSection getMetadata(TextureAtlasSprite sprite) {
         return getMetadata(spriteToAbsolute(sprite.getName()));
     }
 
     public static boolean isEmissive(TextureAtlasSprite sprite) {
         LDLMetadataSection ret = getMetadata(spriteToAbsolute(sprite.getName()));
-        return ret != null && ret.emissive;
+        return ret.emissive;
     }
 
     @Nullable
     public static TextureAtlasSprite getConnection(TextureAtlasSprite sprite) {
         LDLMetadataSection ret = getMetadata(spriteToAbsolute(sprite.getName()));
-        return (ret == null || ret.connection == null) ? null : ModelFactory.getBlockSprite(ret.connection);
+        return ret.connection == null ? null : ModelFactory.getBlockSprite(ret.connection);
     }
 
     public static ResourceLocation spriteToAbsolute(ResourceLocation sprite) {
