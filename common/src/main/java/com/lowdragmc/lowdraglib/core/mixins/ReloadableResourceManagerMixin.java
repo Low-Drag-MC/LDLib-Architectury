d03284115f1ac8872741084d36fa747db648fd28
KilaBash <yefancy@foxmail.com>
2023-11-20 16:28:20 +0900

    Update ReloadableResourceManagerMixin.java

--- common/src/main/java/com/lowdragmc/lowdraglib/core/mixins/ReloadableResourceManagerMixin.java
+++ common/src/main/java/com/lowdragmc/lowdraglib/core/mixins/ReloadableResourceManagerMixin.java
@@ -9,7 +9,6 @@ import net.fabricmc.api.Environment;
 import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.resources.ReloadableResourceManager;
-import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -24,22 +23,22 @@ public abstract class ReloadableResourceManagerMixin {
     @ModifyVariable(method = "createReload", at = @At("HEAD"), index = 4, argsOnly = true)
     private List<PackResources> injectCreateReload(List<PackResources> resourcePacks) {
         if (LDLib.isClient()) {
-            resourcePacks = lowDragLib$injectClientResourcePack(resourcePacks);
+            lowDragLib$injectClientResourcePack();
         }
-        return resourcePacks;
+        
+        var mutableList = new ArrayList<>(resourcePacks);
+        mutableList.add(new CustomResourcePack(LDLib.getLDLibDir(), LDLib.MOD_ID, PackType.CLIENT_RESOURCES));
+
+        return mutableList;
     }
 
     @Unique
-    @NotNull
     @Environment(EnvType.CLIENT)
-    private static List<PackResources> lowDragLib$injectClientResourcePack(List<PackResources> resourcePacks) {
+    private static void lowDragLib$injectClientResourcePack() {
         LDLMetadataSection.clearCache();
         if (LDLib.isRemote()) {
             ShaderTexture.clearCache();
         }
-        var mutableList = new ArrayList<>(resourcePacks);
-        mutableList.add(new CustomResourcePack(LDLib.getLDLibDir(), LDLib.MOD_ID, PackType.CLIENT_RESOURCES));
-        return mutableList;
     }
 
 }
