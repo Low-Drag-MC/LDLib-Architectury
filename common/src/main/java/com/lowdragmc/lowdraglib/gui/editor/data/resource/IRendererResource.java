441dc287be5f9b4355f3e0318a072e3f6c85d2e4
KilaBash <yefancy@foxmail.com>
2024-02-03 18:16:49 +0900

    improve renderer resource

--- common/src/main/java/com/lowdragmc/lowdraglib/gui/editor/data/resource/IRendererResource.java
+++ common/src/main/java/com/lowdragmc/lowdraglib/gui/editor/data/resource/IRendererResource.java
@@ -2,18 +2,31 @@ package com.lowdragmc.lowdraglib.gui.editor.data.resource;
 
 import com.lowdragmc.lowdraglib.client.renderer.IRenderer;
 import com.lowdragmc.lowdraglib.client.renderer.ISerializableRenderer;
+import com.lowdragmc.lowdraglib.client.renderer.impl.IModelRenderer;
 import com.lowdragmc.lowdraglib.gui.editor.ui.ResourcePanel;
 import com.lowdragmc.lowdraglib.gui.editor.ui.resource.IRendererResourceContainer;
 import com.lowdragmc.lowdraglib.gui.editor.ui.resource.ResourceContainer;
 import com.lowdragmc.lowdraglib.gui.widget.Widget;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.Nullable;
 
 public class IRendererResource extends Resource<IRenderer> {
+    public final static String RESOURCE_NAME = "ldlib.gui.editor.group.renderer";
+
+    public IRendererResource() {
+        data.put("empty", IRenderer.EMPTY);
+    }
+
+    @Override
+    public void buildDefault() {
+        data.put("furnace", new IModelRenderer(new ResourceLocation("block/furnace")));
+    }
+
     @Override
     public String name() {
-        return "renderer";
+        return RESOURCE_NAME;
     }
 
     @Override
@@ -36,5 +49,14 @@ public class IRendererResource extends Resource<IRenderer> {
             return ISerializableRenderer.deserializeWrapper(compoundTag);
         }
         return IRenderer.EMPTY;
+    }
+
+    @Override
+    public void deserializeNBT(CompoundTag nbt) {
+        data.clear();
+        data.put("empty", IRenderer.EMPTY);
+        for (String key : nbt.getAllKeys()) {
+            data.put(key, deserialize(nbt.get(key)));
+        }
     }
 }
