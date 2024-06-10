5307444af954706bbaa4d1d8183891cd23a9f655
screret <68943070+screret@users.noreply.github.com>
2024-06-10 14:02:54 +0300

    capture early registries earlier

--- src/main/java/com/lowdragmc/lowdraglib/core/mixins/ReloadableServerResourcesMixin.java
+++ src/main/java/com/lowdragmc/lowdraglib/core/mixins/ReloadableServerResourcesMixin.java
@@ -2,18 +2,28 @@ package com.lowdragmc.lowdraglib.core.mixins;
 
 import com.lowdragmc.lowdraglib.Platform;
 import net.minecraft.commands.Commands;
+import net.minecraft.core.LayeredRegistryAccess;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.server.RegistryLayer;
 import net.minecraft.server.ReloadableServerResources;
+import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.world.flag.FeatureFlagSet;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ReloadableServerResources.class)
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+@Mixin(value = ReloadableServerResources.class, priority = 100)
 public abstract class ReloadableServerResourcesMixin {
-	@Inject(method = "<init>", at = @At("RETURN"))
-	private void init(RegistryAccess.Frozen frozen, FeatureFlagSet featureFlagSet, Commands.CommandSelection commandSelection, int i, CallbackInfo ci) {
-		Platform.FROZEN_REGISTRY_ACCESS = frozen;
+	@Inject(method = "loadResources", at = @At("HEAD"))
+	private static void ldlib$captureEarlyRegistries(ResourceManager resourceManager, LayeredRegistryAccess<RegistryLayer> access,
+                             FeatureFlagSet flags, Commands.CommandSelection commands, int functionCompilationLevel,
+                             Executor gameExecutor, Executor backgroundExecutor,
+                             CallbackInfoReturnable<CompletableFuture<ReloadableServerResources>> cir) {
+		Platform.FROZEN_REGISTRY_ACCESS = access.compositeAccess();
 	}
 }
